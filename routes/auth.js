dc150bb7f0d5af995ddc402e91f40996309d2967
VCDDS <visualcode.dev@hotmail.com>
2026-05-07 09:43:38 +0200

    PSTgreSQL Login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,43 @@
 const express = require("express");
-
 const router = express.Router();
+const { Pool } = require("pg");
 
-router.post("/login", (req, res) => {
-    res.redirect("/dashboard");
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+pool.query(`
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+)
+`);
+
+pool.query(`
+INSERT INTO users (username, password)
+VALUES ('admin', 'admin123')
+ON CONFLICT (username) DO NOTHING
+`);
+
+router.post("/login", express.urlencoded({ extended: true }), async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const result = await pool.query(
+        "SELECT * FROM users WHERE username = $1 AND password = $2",
+        [username, password]
+    );
+
+    if (result.rows.length > 0) {
+        res.redirect("/dashboard");
+    } else {
+        res.send("Falsche Anmeldedaten");
+    }
 });
 
 router.get("/logout", (req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const { Pool } = require("pg");
 const path = require("path");
 const authRoutes = require("./routes/auth");
 const app = express();
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 
 app.use("/", authRoutes);
@@ -27,6 +34,6 @@ app.get("/dashboard.html", (req, res) => {
     res.sendFile(path.join(__dirname, "views", "dashboard.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("http://localhost:3000");
 });
