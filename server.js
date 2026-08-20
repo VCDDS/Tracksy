c86b0a98313502485cc77b7cc25170303ae63faa
VCDDS <visualcode.dev@hotmail.com>
2026-08-20 14:49:09 +0200

    .

--- server.js
+++ server.js
@@ -20,10 +20,15 @@ const mailTransporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: process.env.SMTP_SECURE === "true",
+
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD
-    }
+    },
+
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 20000
 });
 
 function sendTracksyNotification(subject, text){
